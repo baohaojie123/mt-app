26760125a110c78b1fd6187104ad800b22a7ed88
baohaojie <baohaojie@founq.com>
2019-03-19 19:51:45 +0800

    首页

---
+++ components/public/header/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <!--header出口文件-->
+        <el-row :gutter="20" v-bind:gutter="25">
+            <el-col :span="6">
+                <geo></geo>
+            </el-col>
+            <el-col :span="6">
+                <user></user>
+            </el-col>
+            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+</template>
+
+<script>
+import Geo from './geo'
+import User from './user'
+export default {
+  components:{
+    Geo, User
+  }
+}
+</script>
+
+<style lang="scss">
+ @import "@/assets/css/public/layout.scss";
+</style>
